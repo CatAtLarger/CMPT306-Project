8aa2d7c1b4f769a7fe3374398babc1a3d0795bd3
Jonah Konecnik <jok827@usask.ca>
2024-11-05 22:29:54 -0600

    added collision detection and function to change to next ball
    texture will change but not update visually :(

--- cosmic-colliders/ball.gd
+++ cosmic-colliders/ball.gd
@@ -1,20 +1,33 @@
-extends Node2D
+extends RigidBody2D
 
+#for gravity
 var central_mass_position = Vector2.ZERO
 var gravitational_constant = 5000.0
-var rigidbody = RigidBody2D
+
+
+#for collisions
+@export var image_array = [
+	preload("res://Images/Balls/star_red_giant01.png"),
+	preload("res://Images/Balls/star_blue_giant01.png"),
+	preload("res://Images/Balls/star_white_giant04.png")
+]
+var current_image_index = 0
+var sprite: Sprite2D
 
 func _ready():
 	central_mass_position = get_parent().get_node("CentralMass").global_position
-	rigidbody = get_node("RigidBody2D")
+	sprite = get_node("Sprite2D")
+
 
 func _physics_process(delta):
-	apply_gravity(delta, rigidbody)
-	
+	_apply_gravity(delta)
 
-func apply_gravity(delta, rigidbody):
+
+
+func _apply_gravity(delta):
+	
 	var direction = central_mass_position - global_position
-	var distance = rigidbody.direction.length()
+	var distance = direction.length()
 
 	if distance > 0:
 
@@ -22,7 +35,32 @@ func apply_gravity(delta, rigidbody):
 		var force = direction * force_magnitude
 
 		# Apply the gravitational force
-		rigidbody.apply_central_impulse(force * delta)
+		apply_central_impulse(force * delta)
 
 
+func next_ball():
+	# Double the size of the ball
+	scale *= 2
+
+	# Move to the next image in the array
+
+	current_image_index += 1
+	if current_image_index < image_array.size():
+		print("Texture before:", sprite.texture)
+		sprite.texture = image_array[current_image_index]
+		print("Texture after:", sprite.texture)
 	
+	
+	else:
+		current_image_index = image_array.size() - 1  # Cap at the last image
+
+	var collision_shape = $CollisionShape2D.shape
+	if collision_shape is CircleShape2D:
+		collision_shape.radius *= 2
+		
+		
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	var collision_object = area.get_parent()
+	if is_in_group("balls"):
+		collision_object.queue_free()
+		next_ball()
