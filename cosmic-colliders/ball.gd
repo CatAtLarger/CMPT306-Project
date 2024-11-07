9588ad753bf27a212a41746e05011030fb71c1ab
Jonah Konecnik <jok827@usask.ca>
2024-11-06 21:26:19 -0600

    instantiated each celestial body as own scene

--- cosmic-colliders/ball.gd
+++ cosmic-colliders/ball.gd
@@ -2,13 +2,25 @@ extends RigidBody2D
 
 #for gravity
 var central_mass_position = Vector2.ZERO
-var gravitational_constant = 5000.0
+@export var gravitational_constant = 5000.0
 
-
-
+#for loading next ball's scene after collision
+@onready var celestial_objects = [
+	preload("res://moon.tscn"),
+	preload("res://dwarf_planet.tscn"),
+	preload("res://planet.tscn"),
+	preload("res://gas_giant.tscn"),
+	preload("res://red_dwarf.tscn"),
+	preload("res://blue_star.tscn"),
+	preload("res://white_giant.tscn")
+	
+]
+@export var celestial_index = 0
 
 func _ready():
 	central_mass_position = get_parent().get_parent().get_node("CentralMass").global_position
+
+	
 
 
 
@@ -31,35 +43,40 @@ func _apply_gravity(delta):
 		apply_central_impulse(force * delta)
 
 
-func next_ball():
-	# Double the size of the ball
-	scale *= 2
+func next_ball(collision_object):
+	
+		# if image is the same then must be same ball
 
-	var collision_shape = $CollisionShape2D.shape
-	if collision_shape is CircleShape2D:
-		collision_shape.radius *= 2
+		if collision_object.get_child(1).texture == get_child(1).texture:
+			
+			var next_ball_index = celestial_index + 1
+			var next_ball_scene = celestial_objects[next_ball_index]
+			
+			
+			if next_ball_scene:
+				
+				var new_ball = next_ball_scene.instantiate()
+
+				get_parent().add_child(new_ball)
+					
+				new_ball.position = collision_object.position
+				collision_object.queue_free()
+				call_deferred("queue_free")
+				
+			else:
+				push_error("Cannot get get next celestial_object!")
+		
 		
 		
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	print("Hit")
+	
 	var collision_object = area.get_parent()
 	
 	if is_in_group("balls"):
-		# if image is the same then must be same ball
-		if collision_object.get_child(1).texture == get_child(1).texture:
-			
-			var next_ball_path = collision_object.get_meta("next_ball_path")
-			if next_ball_path != null:
-				pass
-				#var next_ball_scene = load(String(next_ball_path))
-				#if next_ball_scene:
-					#var new_ball = next_ball_scene.instance()
-					#
-					#print(new_ball)
-					#
-					#get_parent().add_child(new_ball)
-					#
-					#new_ball.position = collision_object.position
-					
-			collision_object.queue_free()
 		
+		next_ball(collision_object)
+		
+		
+
+func _on_orbit_area_exited(area: Area2D) -> void:
+	pass # Replace with function body.
