d3ba451c7633f44dbdfeaf1651819305ee044366
Jonah Konecnik <jok827@usask.ca>
2024-11-06 22:23:31 -0600

    added background music

--- cosmic-colliders/BallPhysics.gd
+++
@@ -1,8 +0,0 @@
-extends RigidBody2D
-
-var lock_movement = false  # Custom variable to control movement
-
-func _physics_process(delta):
-	if lock_movement:
-		# Prevent movement when locked
-		return
