b508ee9c70568592338a79cedf2123975bc56d6e
Jonah Konecnik <jok827@usask.ca>
2024-11-07 20:51:05 -0600

    finished area2d collision for prototype

--- cosmic-colliders/orbit.gd
+++ cosmic-colliders/orbit.gd
@@ -3,18 +3,24 @@ extends Area2D
 # Set boundary for detecting out-of-bounds
 @onready var boundary = $CollisionShape2D  # Adjust as necessary
 
+var boundary_radius = 80 # boundary.shape.radius * boundary.scale.x
+
+func _ready() -> void:
+	push_warning("Boundary_radius hard coded!")
+
 func _on_timer_timeout():
 	for body in get_overlapping_bodies():
 
 		if body is RigidBody2D:
-			var boundary_radius = 80 # boundary.shape.radius * boundary.scale.x
 			if body.global_position.distance_to(global_position) > boundary_radius:
-				print(body.global_position.distance_to(global_position), "is greater than", boundary_radius)
-				game_over()
-				return
-			else:
-				print("False")
+				check_game_over()
+			
 
-func game_over():
-	print("Game Over!")
-	# Additional game over logic here
+func check_game_over():
+	await get_tree().create_timer(3).timeout
+	
+	for body in get_overlapping_bodies():
+		if body is RigidBody2D:
+ 
+			if body.global_position.distance_to(global_position) > boundary_radius:
+				print("Game Over")
