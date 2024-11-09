b282c94fc47b8336e91a56d49bae9ddbb385b920
GogeBat21 <hti315@usask.ca>
2024-11-08 22:47:36 -0600

    Added so that now once balls combine scoreupdates, seems to work for all except the white giant?

--- cosmic-colliders/Scripts/Autoscript.gd
+++ cosmic-colliders/Scripts/Autoscript.gd
@@ -1,2 +1,12 @@
 extends Node
+
+# Global score variable
 var score: int = 0
+
+# Function to update the score label display
+func update_score_display():
+	if has_node("/root/Root/Score"):
+		var score_label = get_node("/root/Root/Score")
+		score_label.text = "Score: " + str(score)
+	else:
+		push_error("Score label not found.")
--- cosmic-colliders/Scripts/ball.gd
+++ cosmic-colliders/Scripts/ball.gd
@@ -1,15 +1,12 @@
 extends RigidBody2D
 
-#for gravity
+# for gravity
 var central_mass_position := Vector2(577,339)
 @export var gravitational_constant := 5000.0
 
-
 @onready var orbit = load("res://orbit.tscn")
 
-
-
-#for loading next ball's scene after collision
+# for loading next ball's scene after collision
 @onready var celestial_objects = [
 	load("res://Ball_Scenes/moon.tscn"),
 	load("res://Ball_Scenes/dwarf_planet.tscn"),
@@ -21,96 +18,57 @@ var central_mass_position := Vector2(577,339)
 ]
 @export var celestial_index = 0
 
-
-#for debugging
+# for debugging
 var dragging = false
 
 func _ready() -> void:
-	
-	var central_mass := get_node_or_null("CentralMass")
+	var central_mass = get_node_or_null("CentralMass")
 	
 	if not central_mass == null:
-		
 		central_mass_position = central_mass.position
-		
 	else:
-		push_warning("central_mass_position is hardcoded, cannot get \"Central Mass\" object" )
+		push_warning("central_mass_position is hardcoded, cannot get \"Central Mass\" object")
 
-
-	#debugging
-	#set_process_input(true)
-	
-
-
-#func _input(event: InputEvent) -> void:
-	#if event is InputEventMouseButton:
-		#if event.button_index == MOUSE_BUTTON_LEFT:
-			#if event.pressed:
-				#if position.distance_to(get_global_mouse_position()) < 20:
-					#dragging = true
-				#else:
-					#dragging = false
-					
-					
 func _physics_process(delta):
-	
 	_apply_gravity(delta)
-	
-	#for debugging
-	#if dragging:
-		#global_position = get_global_mouse_position()
-
-
 
 func _apply_gravity(delta):
-	
 	var direction = central_mass_position - global_position
 	var distance = direction.length()
 
 	if distance > 0:
-
 		var force_magnitude = gravitational_constant / pow(distance, 2)
 		var force = direction * force_magnitude
-
 		# Apply the gravitational force
 		apply_central_impulse(force * delta)
 
-
 func next_ball(collision_object):
-	
-	
-		# if image is the same then must be same ball
-		if collision_object.get_child(1).texture == get_child(1).texture:
-			
-			var next_ball_index = celestial_index + 1
-			
-			
+	# if image is the same then must be same ball
+	if collision_object.get_child(1).texture == get_child(1).texture:
+		var next_ball_index = celestial_index + 1
 
-			#if not last ball
-			if next_ball_index < celestial_objects.size() :
-				#make new ball
-				var next_ball_scene = celestial_objects[next_ball_index]
-				var new_ball = next_ball_scene.instantiate()
-				get_parent().call_deferred("add_child", new_ball)
-				new_ball.position = collision_object.position
+		# if not last ball
+		if next_ball_index < celestial_objects.size():
+			# make new ball
+			var next_ball_scene = celestial_objects[next_ball_index]
+			var new_ball = next_ball_scene.instantiate()
+			get_parent().call_deferred("add_child", new_ball)
+			new_ball.position = collision_object.position
+
+			# Update score by adding double the current ball's value
+			var ball_value = self.get_meta("value")  # Retrieve metadata value
+			if ball_value != null:
+				Autoscript.score += ball_value * 2  # Add twice the current ball's value
+				Autoscript.update_score_display()  # Update score display
+			else:
+				push_warning("Ball instance has no metadata value.")
 				
-				
-				#attach orbit to new child
-				#orbit.connect("area_exited", new_ball, _on_orbit_area_exited)
+		# Free the colliding balls after combining
+		collision_object.queue_free()
+		call_deferred("queue_free")
 
-
-			collision_object.queue_free()
-			call_deferred("queue_free")
-			
-		
-		
-		
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	
 	var collision_object = area.get_parent()
-
 	if is_in_group("balls"):
-
-		if not is_queued_for_deletion() && not collision_object.is_queued_for_deletion():
-			
+		if not is_queued_for_deletion() and not collision_object.is_queued_for_deletion():
 			next_ball(collision_object)
