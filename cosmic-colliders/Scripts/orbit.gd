3f90b094d16356ddd2534d35f8ca53124f6d2bb4
Jonah Konecnik <jok827@usask.ca>
2024-12-05 18:12:12 -0600

    added code to detect if balls have left area

--- cosmic-colliders/Scripts/orbit.gd
+++ cosmic-colliders/Scripts/orbit.gd
@@ -2,10 +2,28 @@ extends Area2D
 
 @onready var boundary = $CollisionShape2D  # Adjust as necessary
 
-
+var boundary_radius = 0
 
 func _ready() -> void:
-	var boundary_radius = boundary.shape.radius
-	print(boundary_radius)
+	boundary_radius = boundary.shape.radius * boundary.scale.x
 
- 
+
+func _on_balls_child_entered_tree(node: Node) -> void:
+	var ball_area = node.get_child(2)
+	ball_area.connect("area_exited",on_area_exited)
+
+func on_area_exited(area: Area2D):
+	$Timer.start()
+
+
+func _on_timer_timeout() -> void:
+	var current_balls = get_parent().get_node("Balls").get_children()
+	
+
+	for ball in current_balls:
+		var ball_radius = ball.get_child(0).shape.radius * ball.get_child(0).scale.x
+		var distance_to_center = ball.global_position.distance_to($CollisionShape2D.global_position)
+		print(ball_radius)
+		if distance_to_center > boundary_radius - ball_radius:
+			print("Game Over")
+			
