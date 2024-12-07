8a8c1aa629e4d31d22beed041b9b7a46730bc549
GogeBat21 <hti315@usask.ca>
2024-12-06 19:37:58 -0600

    Shows final score for gaem menu

--- cosmic-colliders/Scripts/orbit.gd
+++ cosmic-colliders/Scripts/orbit.gd
@@ -1,13 +1,12 @@
 extends Area2D
 
-@onready var boundary = $CollisionShape2D  # Adjust as necessary
-@onready var game_over_menu = get_tree().root.get_node("Main Scene/GameOverMenu")  # Reference to the GameOverMenu node
+@onready var boundary = $CollisionShape2D
+@onready var game_over_menu = get_tree().root.get_node("Main Scene/GameOverMenu")
 
 var boundary_radius = 0
 
 func _ready() -> void:
 	boundary_radius = boundary.shape.radius * boundary.scale.x
-
 
 func _on_balls_child_entered_tree(node: Node) -> void:
 	var ball_area = node.get_child(2)
@@ -17,22 +16,19 @@ func on_area_exited(area: Area2D):
 	if $Timer.is_stopped():
 		$Timer.start()
 
-
 func _on_timer_timeout() -> void:
 	var current_balls = get_parent().get_node("Balls").get_children()
-
 	for ball in current_balls:
 		var ball_radius = ball.get_child(0).shape.radius * ball.get_child(0).scale.x
 		var distance_to_center = ball.global_position.distance_to($CollisionShape2D.global_position)
 		if (distance_to_center > boundary_radius - ball_radius) and ball.has_collided:
-			show_game_over()  # Trigger the Game Over logic
+			show_game_over()
 			return
 
-
 func show_game_over():
-	get_tree().paused = true  # Pause the game
+	get_tree().paused = true
 	if game_over_menu:
-		game_over_menu.visible = true  # Display the Game Over menu
+		game_over_menu.visible = true
+		Autoscript.update_game_over_score()  # Update the score label
 	else:
 		push_error("GameOverMenu node not found!")
-	
